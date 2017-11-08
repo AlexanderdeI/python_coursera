ebe4542dd0b92c5813145afe14c8ea3706a14f3f
AlexanderdeI <semionchik.a.g@gmail.com>
2017-11-09 01:20:28 +0300

    week_02: task storage.py

--- week_02/tasks/storage.py
+++ week_02/tasks/storage.py
@@ -1,4 +1,7 @@
-import argparse, json, os, tempfile
+import argparse
+import json
+import os
+import tempfile
 
 
 storage_path = os.path.join(tempfile.gettempdir(), 'storage_data')
@@ -9,6 +12,24 @@ with open(storage_path, 'w') as f:
     for key, value in storage.items():
         f.write("{}: {},\n".format(key, value))
 
+
+def find_key(storage_path, *args):
+    items = []
+    with open(storage_path, 'r') as f:
+        text = (f.read().strip()).split(",\n")
+        for x in text:
+            items.append(x.split(':'))
+    storage = {k:v for k,v in items}
+    for key in args:
+        return storage.get(key)
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument("key")
-parser.add_argument("val")
+parser.add_argument("keys")
+parser.add_argument("-k", "--key", action="store_true",
+                    help="returns value of key or None")
+args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    print(find_key(storage_path, args.keys))
